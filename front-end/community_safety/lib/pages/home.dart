731ceed634b4cceee5f499367defa03b3e953909
Jaycey <jayceybrugge23@gmail.com>
2025-03-26 14:23:46 +0100

    extracted mainCard method and ignored deprecated member use

--- front-end/community_safety/lib/pages/home.dart
+++ front-end/community_safety/lib/pages/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -16,107 +18,111 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
       appBar: appBar(),
-      body: Center(
-        child: Container(
-          width: screenWidth * 0.9,
-          height: screenHeight * 0.43,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(20),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.3),
-                blurRadius: 5,
-                spreadRadius: 3,
-                offset: Offset(0, 0),
+      body: mainCard(screenWidth, screenHeight, cardWidth, buttonWidth),
+    );
+  }
+
+  Center mainCard(double screenWidth, double screenHeight, double cardWidth, double buttonWidth) {
+    return Center(
+      child: Container(
+        width: screenWidth * 0.9,
+        height: screenHeight * 0.43,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.3),
+              blurRadius: 5,
+              spreadRadius: 3,
+              offset: Offset(0, 0),
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(left: 20, top: 20, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Plan your route',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              SizedBox(height: 10),
+              SizedBox(
+                width: cardWidth * 0.9,
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.near_me),
+                    labelText: 'Start location',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              SizedBox(
+                width: cardWidth * 0.9,
+                child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.location_on),
+                    labelText: 'Destination',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Wrap(
+                spacing: 10,
+                runSpacing: 10,
+                children: [
+                  _buildFilterButton(
+                    Icons.lightbulb,
+                    "Illuminated streets",
+                    Color(yellowColor),
+                    buttonWidth,
+                  ),
+                  _buildFilterButton(
+                    Icons.groups,
+                    "Crowded spaces",
+                    Color(blueColor),
+                    buttonWidth,
+                  ),
+                  _buildFilterButton(
+                    Icons.videocam,
+                    "Security cameras",
+                    Color(greenColor),
+                    buttonWidth,
+                  ),
+                  _buildFilterButton(
+                    Icons.no_drinks,
+                    "Alcohol banned area",
+                    Color(redColor),
+                    buttonWidth,
+                  ),
+                ],
+              ),
+              SizedBox(height: 15),
+              SizedBox(
+                width: cardWidth * 0.9,
+                child: ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.search, color: Colors.white),
+                  label: Text("Find safest route"),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(blueColor),
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
               ),
             ],
-          ),
-          child: Padding(
-            padding: EdgeInsets.only(left: 20, top: 20, right: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Plan your route',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                ),
-                SizedBox(height: 10),
-                SizedBox(
-                  width: cardWidth * 0.9,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.near_me),
-                      labelText: 'Start location',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10),
-                SizedBox(
-                  width: cardWidth * 0.9,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.location_on),
-                      labelText: 'Destination',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Wrap(
-                  spacing: 10,
-                  runSpacing: 10,
-                  children: [
-                    _buildFilterButton(
-                      Icons.lightbulb,
-                      "Illuminated streets",
-                      Color(yellowColor),
-                      buttonWidth,
-                    ),
-                    _buildFilterButton(
-                      Icons.groups,
-                      "Crowded spaces",
-                      Color(blueColor),
-                      buttonWidth,
-                    ),
-                    _buildFilterButton(
-                      Icons.videocam,
-                      "Security cameras",
-                      Color(greenColor),
-                      buttonWidth,
-                    ),
-                    _buildFilterButton(
-                      Icons.no_drinks,
-                      "Alcohol banned area",
-                      Color(redColor),
-                      buttonWidth,
-                    ),
-                  ],
-                ),
-                SizedBox(height: 15),
-                SizedBox(
-                  width: cardWidth * 0.9,
-                  child: ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.search, color: Colors.white),
-                    label: Text("Find safest route"),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(blueColor),
-                      foregroundColor: Colors.white,
-                      padding: EdgeInsets.symmetric(vertical: 15),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
           ),
         ),
       ),
